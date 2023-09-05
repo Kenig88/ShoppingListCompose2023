383c38e5e98609e6170c328c914550b99f9fd4af
s.krotov <14bassdrumand88@gmail.com>
2023-09-05 10:53:24 +0300

    1# entities + dao

---
+++ app/src/main/java/com/kenig/shoppinglistcompose2023/data/MainDb.kt
@@ -0,0 +1,9 @@
+package com.kenig.shoppinglistcompose2023.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [ShoppingListItem::class, AddItem::class, NoteItem::class], version = 1)
+abstract class MainDb : RoomDatabase() {
+    abstract val shoppingListDao: ShoppingListDao
+}
---
+++ app/src/main/java/com/kenig/shoppinglistcompose2023/data/ShoppingListDao.kt
@@ -0,0 +1,16 @@
+package com.kenig.shoppinglistcompose2023.data
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ShoppingListDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertItem(item: ShoppingListItem)
+
+    @Delete
+    suspend fun deleteItem(item: ShoppingListItem)
+
+    @Query("SELECT * FROM shop_list_name")
+    fun getAllItems(): Flow<List<ShoppingListItem>>
+}
